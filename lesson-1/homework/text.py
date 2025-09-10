e52b958a60a12bbd54ffb609c31fb64b724544fb
elshod16 <elshodtoshkentov@gmail.com>
2025-09-10 11:55:23 +0500

    Create text.py

---
+++ lesson-1/homework/text.py
@@ -0,0 +1,20 @@
+#1
+a=int(input('kvadratning uzunligi:'))
+print(4*a) # a ning perimetri 
+print(a**2) # a ning yuzi 
+#2
+import math 
+diametr=float(input('diametr kiriting:'))#diametr kiritish 
+uzunlik=math.pi*diametr #matematik pi dan foydalanish 
+print(f'doiraning  uzunligi:{uzunlik}') #natija
+#3
+a=int(input('a ning qiymati:'))
+b=int(input('b ning qiymati:'))
+print (a*b/2)   # a va b sonlarining o'rtcha qiymati
+#4
+a=int(input('a ning qiymati:'))
+b=int(input('b ning qiymati:'))
+print(a+b)
+print(a*b)
+print(a**2)
+print(b**2)
